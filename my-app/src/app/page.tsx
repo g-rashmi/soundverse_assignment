ba50833d49c956755d639f71fe485e61eb2bab9a
g-rashmi <rashmi.gupta7018@gmail.com>
2024-07-08 16:37:50 +0530

    bug-fix

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,16 +1,16 @@
 import ChallengeCard from '@/components/card'
 import React from 'react'
 
-
+import { Suspense } from 'react'
 
 function page() {
   return (
-    <div className="m-10 text-center w-30">
+    <Suspense><div className="m-10 text-center w-30">
 
     <div>  <ChallengeCard/></div>
 
    
-    </div>
+    </div></Suspense>
   )
 }
 
